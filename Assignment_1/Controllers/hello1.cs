7fe9ae9c71c91120f11e4cde532df11e7ea29b16
Camoy Phillips <n01496384@humber.ca>
2025-01-31 22:36:16 -0500

    Add project files.

---
+++ Assignment_1/Controllers/hello1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_1.Controllers
+{
+    [Route("api/hello1")]
+    [ApiController]
+
+    public class hello1
+    {
+        /// <summary>
+        /// Gets a welcome message.
+        /// </summary>
+        /// <returns>
+        /// A string "Welcome to 5125!".
+        /// </returns>
+        /// <example>
+        /// GET http://localhost:xx/api/q1/welcome
+        /// Response: "Welcome to 5125!"
+        /// </example>
+        [HttpGet("welcome")]
+        public IActionResult GetWelcomeMessage()
+        {
+            return Ok("Welcome to 5125!");
+
+        }
+    }
+}
